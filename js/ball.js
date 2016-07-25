b6b108dbf1d178abf066c55758a69114edc05072
jiangliu2u <jiangliu2u@gmail.com>
2016-07-25 14:35:06 +0800

    好多球

--- js/ball.js
+++ js/ball.js
@@ -1,11 +1,15 @@
-function Ball(radius,color){
-    if(radius === undefined) {radius = 40;}
-    if(color === undefined){color = '#00ff00';}
+function Ball(radius, color) {
+    if (radius === undefined) {
+        radius = 40;
+    }
+    if (color === undefined) {
+        color = '#00ff00';
+    }
     this.x = 0;
     this.y = 0;
     this.vx = 0;
     this.vy = 0;
-    this.gravity=0;
+    this.gravity = 0;
     this.radius = radius;
     this.rotation = 0;
     this.mass = 1;
@@ -18,17 +22,17 @@ function Ball(radius,color){
 
 }
 
-Ball.prototype.draw = function(context){
+Ball.prototype.draw = function (context) {
     //drawGrid(context,'white',20,20);
     context.save();
-    context.translate(this.x,this.y);
+    context.translate(this.x, this.y);
     context.rotate(this.rotation);
-    context.scale(this.scaleX,this.scaleY);
+    context.scale(this.scaleX, this.scaleY);
     context.lineWidth = this.lineWidth;
     context.fillStyle = this.color;
     context.strokeStyle = this.color;
     context.beginPath();
-    context.arc(0,0,this.radius,0,Math.PI*2,false);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2, false);
     context.closePath();
     context.fill();
     context.stroke();
@@ -36,12 +40,12 @@ Ball.prototype.draw = function(context){
 }
 
 //得到球体的左上角坐标
-Ball.prototype.getBounds = function(){
+Ball.prototype.getBounds = function () {
     return {
         x: this.x - this.radius,
         y: this.y - this.radius,
-        width: this.radius*2,
-        height: this.radius*2
+        width: this.radius * 2,
+        height: this.radius * 2
     };
 
 }
